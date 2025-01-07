9ca61336630803b344acc4274a2b296ba6f5a25a
JM <jo3tech@outlook.com>
2025-01-07 16:36:18 +0000

    Updated comments.

--- src/display_manager.h
+++ src/display_manager.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Morgritech
+// Copyright (C) 2025 Morgritech
 //
 // Licensed under GNU General Public License v3.0 (GPLv3) License.
 // See the LICENSE file in the project root for full license details.
