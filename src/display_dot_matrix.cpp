bfad958af5ea9aa4d4a0ce2be8c8e9e4fb000998
JM <jo3tech@outlook.com>
2025-06-30 07:22:01 +0100

    Restructured for dependency injection.

---
+++ src/display_dot_matrix.cpp
@@ -0,0 +1,65 @@
+// Copyright (C) 2025 Morgritech
+//
+// Licensed under GNU General Public License v3.0 (GPLv3) License.
+// See the LICENSE file in the project root for full license details.
+
+/// @file display_dot_matrix.cpp
+/// @brief Class that handles visual output to the (dot-matrix) display.
+
+#include "display_dot_matrix.h"
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+#include "common_types.h"
+#include "configuration.h"
+
+namespace mtmotor_jig {
+
+DisplayDotMatrix::DisplayDotMatrix(LiquidCrystal& display, Configuration& configuration)
+    : display_(display), configuration_(configuration) {}
+
+DisplayDotMatrix::~DisplayDotMatrix() {}
+
+void DisplayDotMatrix::Draw(common::ControlMode control_mode, common::ControlAction control_action,
+                          const String& status) {
+  switch (control_mode) {
+    case common::ControlMode::kSplashScreen: {
+      DrawScreenItems(configuration_.kSplashScreenMenuItems_);
+      delay(configuration_.kSplashScreenDelay_ms_);
+      break;
+    }
+    case common::ControlMode::kHomeScreen: {
+      DrawScreenItems(configuration_.kHomeScreenMenuItems_);
+      break;
+    }
+    case common::ControlMode::kContinuousMenu: {
+      cursor_position_y_ = configuration_.kContinuousMenuCursorPositionY_;
+      break;
+    }
+    case common::ControlMode::kOscillateMenu: {
+      cursor_position_y_ = configuration_.kOscillateMenuCursorPositionY_;
+      break;
+    }
+  }
+
+  if (control_action != common::ControlAction::kIdle || control_mode == common::ControlMode::kHomeScreen) {
+    display_.setCursor(0, configuration_.kStatusBarCursorPositionY_);
+    display_.print(status);
+    display_.setCursor(0, cursor_position_y_);
+  }
+}
+
+template <size_t Size>
+void DisplayDotMatrix::DrawScreenItems(const String (&screen_items)[Size]) {
+  display_.clear(); //Clear the display and position the cursor in the upper-left corner (0, 0).
+
+  uint8_t i = 0;
+  for (auto screen_item : screen_items) {
+    display_.setCursor(0, i);
+    display_.print(screen_item);
+    i++;
+  }
+}
+
+} // namespace mtmotor_jig
---
+++ src/display_dot_matrix.h
@@ -0,0 +1,52 @@
+// Copyright (C) 2025 Morgritech
+//
+// Licensed under GNU General Public License v3.0 (GPLv3) License.
+// See the LICENSE file in the project root for full license details.
+
+/// @file display_dot_matrix.h
+/// @brief Class that handles visual output to the (dot-matrix) display.
+
+#pragma once
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+#include "common_types.h"
+#include "configuration.h"
+
+namespace mtmotor_jig {
+
+/// @brief The Display (Dot Matrix) class.
+class DisplayDotMatrix {
+ public:
+
+  /// @brief Construct a Display (Dot Matrix) object.
+  DisplayDotMatrix(LiquidCrystal& display, Configuration& configuration = Configuration::GetInstance());
+
+  /// @brief Destroy the Display (Dot Matrix) object.
+  ~DisplayDotMatrix();
+
+  /// @brief Draw items to the display based on the control mode.
+  /// @param control_mode The control mode.
+  /// @param status The status message to display.
+  void Draw(common::ControlMode control_mode, common::ControlAction control_action, const String& status);
+
+ private:
+
+  /// @brief Draw items to the display.
+  /// @tparam Size The size of the screen items.
+  /// @param screen_items The screen items to draw.
+  template <size_t Size>
+  void DrawScreenItems(const String (&screen_items)[Size]);
+
+  /// @brief The LCD display.
+  LiquidCrystal& display_;
+
+  /// @brief Configuration settings.
+  Configuration& configuration_;
+
+  /// @brief The current cursor position along the y-axis.
+  uint8_t cursor_position_y_ = configuration_.kDefaultCursorPositionY_;
+};
+
+} // namespace mtmotor_jig
